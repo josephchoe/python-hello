4bbabccbb02aea2fc07147a4daec25f0852aa1dc
Joseph Choe <joseph@josephchoe.com>
2018-08-20 15:26:12 -0700

    Youtube

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 To run:
 
     docker run --rm -v "$PWD":/var/task lambci/lambda:python3.6 lambda_function.lambda_handler '{ "name": "world" }'
+
+AWS API Gateway
+
+    https://cqwybngfb0.execute-api.us-east-1.amazonaws.com/production/helloWorld?name=Test
--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,36 @@
+import os
 import json
+import argparse
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+DEVELOPER_KEY = os.environ['DEVELOPER_KEY']
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
 
 def lambda_handler(event, context):
-  message = hello(event['name'])
-  body = {
-    "message": message
-  }
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY, cache_discovery=False)
 
-  response = {
-    "statusCode": 200,
-    "body": json.dumps(body)
-  }
+  search_response = youtube.search().list(
+    q='penguin',
+    channelId='UCwez9XDNV_wS0WNDZteXjgw',
+    part='id,snippet',
+    maxResults=25
+  ).execute()
 
-  return response
+  videos = []
+  channels = []
 
-def hello(name):
-  return "Hello, %s!" % name
+  for search_result in search_response.get('items', []):
+    if search_result['id']['kind'] == 'youtube#video':
+      videos.append('%s (%s)' % (search_result['snippet']['title'],
+                                 search_result['id']['videoId']))
+    elif search_result['id']['kind'] == 'youtube#channel':
+      channels.append('%s (%s)' % (search_result['snippet']['title'],
+                                   search_result['id']['channelId']))
+
+  print 'Videos:\n', '\n'.join(videos), '\n'
+  print 'Channels:\n', '\n'.join(channels), '\n'
+
+  return "Test"
